9015bc84aca041a0f4f341250f4ce7b670433f0f
Josephine Green <75851942+Fire-fly630@users.noreply.github.com>
2025-05-12 20:35:04 -0500

    Add files via upload

---
+++ responses.py
@@ -0,0 +1,7 @@
+#PARI Responses
+import random
+
+Greeting = ["Hello", "Salutations", "What do you need", "Hello, how are you?", "How can I help?"]
+
+Goodbye = ["Glad I could help", "Good-Bye", "Until we meet again", "Have a good day"]
+
